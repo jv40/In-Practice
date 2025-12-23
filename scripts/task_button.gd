5395b4c452ce0d2cc0ee18409c9e4d989342d627
Jonathan Vasquez <vasquez_jonathan@wheatoncollege.edu>
2025-12-22 22:57:56 -0500

    Task button line appears when task checked off

--- scripts/task_button.gd
+++ scripts/task_button.gd
@@ -9,3 +9,24 @@ extends Button
 #
 		#if event.pressed and event.button_index == "mouse_right_click":
 			#print("Right!")
+
+@onready var check_box: CheckBox = $CheckBox
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func is_task_done():
+	if check_box.button_pressed == true:
+		return true
+	else:
+		return false
+
+#func _on_mouse_entered() -> void:
+	#if is_task_done() == true:
+		#animation_player.play("completed")
+	#else:
+		#animation_player.play("RESET")
+
+func _process (delta: float) -> void:
+	if is_task_done() == true:
+		animation_player.play("completed")
+	else:
+		animation_player.play("RESET")
