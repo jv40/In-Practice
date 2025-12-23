6560398ec128ddcb27fca8f4296afdba062c57b0
Jonathan Vasquez <vasquez_jonathan@wheatoncollege.edu>
2025-12-22 20:58:45 -0500

    Button identification function (which_botton) created

--- scripts/button.gd
+++ scripts/button.gd
@@ -2,8 +2,29 @@ extends Button
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+@onready var button: Button = $"."
+
+# Do you disable the font or not
+# Yes = true, No = false
+var do_i_disable: bool = false
+
+func _ready() -> void:
+	which_button()
+	animation_player.play("disable_font")
+	
+
+func which_button():
+	
+	if button.text == "Load":
+		do_i_disable = true
+	elif button.text == "New Data":
+		do_i_disable = true
+	elif button.text == "Settings":
+		do_i_disable = true
+	elif button.text == "Art":
+		do_i_disable = true
+
 func _on_pressed() -> void:
-	print ("Button pressed")
 	animation_player.play("press")
 
 func _on_mouse_entered() -> void:
