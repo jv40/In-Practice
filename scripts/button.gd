407d493e10cbb6f11d09bd246daf78275a725aa6
Jonathan Vasquez <vasquez_jonathan@wheatoncollege.edu>
2025-12-26 22:18:41 -0500

    BUG: Home button text does not disappear on scene change

--- scripts/button.gd
+++ scripts/button.gd
@@ -4,26 +4,60 @@ extends Button
 
 @onready var button: Button = $"."
 
+#@onready var parent = get_parent()
+
 # Do you disable the font or not
 # Yes = true, No = false
 var do_i_disable: bool = false
+var original_text: String = ""
 
 func _ready() -> void:
+	add_to_group("menu_buttons")  # Add this line!
+	
+	original_text = text
+	
+	animation_player = get_node_or_null("AnimationPlayer")
+	if not animation_player:
+		print("animation_player not found")
+		return
+	
 	which_button()
-	animation_player.play("disable_font")
+	
+	if do_i_disable:
+		animation_player.play("disable_font")
+
+func _process(delta: float):
+	pass
+	#animation_player.play("disable_font")
 	
 
-func which_button():
+func disable_font_func():
+	animation_player = get_node_or_null("AnimationPlayer")
+	if not animation_player:
+		return
 	
-	if button.text == "Load":
+	which_button()
+	if do_i_disable:
+		print(text, " - animation playing")
+		animation_player.play("disable_font")
+		
+	else:
+		print(text, " - NOT disabling (do_i_disable is false)")
+
+func which_button():
+	do_i_disable = false  # Reset it first!
+	
+	if original_text == "Load":
+		print ("Load button disabling")
 		do_i_disable = true
-	elif button.text == "New_Data":
+	elif original_text == "New_Data":
 		do_i_disable = true
-	elif button.text == "Settings":
+	elif original_text == "Settings":
 		do_i_disable = true
-	elif button.text == "Art":
+	elif original_text == "Art":
 		do_i_disable = true
-	elif button.text == "Home":
+	elif original_text == "Home":
+		print ("Home Button Disabling")
 		do_i_disable = true
 
 func change_scene(nextScene):
@@ -31,15 +65,17 @@ func change_scene(nextScene):
 		await get_tree().create_timer(0.2).timeout
 		get_tree().change_scene_to_file("res://scenes/task_menu.tscn")
 	elif nextScene == "Home":
+		var current_scene = get_tree().current_scene
+		if current_scene.scene_file_path == "res://scenes/home_menu.tscn":
+			return
 		await get_tree().create_timer(0.2).timeout
 		get_tree().change_scene_to_file("res://scenes/home_menu.tscn")
 
 func _on_pressed() -> void:
 	animation_player.play("press")
-	change_scene(button.text)
+	change_scene(original_text)
 
 func _on_mouse_entered() -> void:
-	print ("mouse enter")
 	animation_player.play("hover")
 
 #func _on_mouse_exited() -> void:
