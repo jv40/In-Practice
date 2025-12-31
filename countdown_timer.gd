776931d7369b559288a9f1981508969708b09e37
Jonathan Vasquez <vasquez_jonathan@wheatoncollege.edu>
2025-12-30 23:47:30 -0500

    Timer button press animation revised

--- countdown_timer.gd
+++ countdown_timer.gd
@@ -1,6 +1,10 @@
 extends Control
 
 @onready var timer: Timer = $Timer
+const START_BUTTON = preload("res://assets/Start Button-8.png")
+const PAUSE_BUTTON = preload("res://assets/Pause Button-8.png")
+@onready var timer_button: Button = $"Timer Button"
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	Reset_Timer()
@@ -29,16 +33,39 @@ func Reset_Timer():
 
 func _on_timer_button_pressed() -> void:
 	#timer.paused = !timer.paused
+	
 	if timer.is_stopped() == true:
 		#Start timer if not running
 		timer.start()
+		var normal_style = timer_button.get_theme_stylebox("normal")
+		var hover_style = timer_button.get_theme_stylebox("hover")
+		if normal_style is StyleBoxTexture:
+			normal_style.texture = PAUSE_BUTTON
+			hover_style.texture = PAUSE_BUTTON
+			timer_button.position.y += 5
+			animation_player.play("paused_button")
 	
 	elif timer.paused:
 		#Resume
 		timer.paused = false
+		var normal_style = timer_button.get_theme_stylebox("normal")
+		var hover_style = timer_button.get_theme_stylebox("hover")
+		if normal_style is StyleBoxTexture:
+			normal_style.texture = PAUSE_BUTTON
+			hover_style.texture = PAUSE_BUTTON
+			timer_button.position.y += 5
+			animation_player.play("paused_button")
+			
 	else:
 		#Pause
 		timer.paused = true
+		var normal_style = timer_button.get_theme_stylebox("normal")
+		var hover_style = timer_button.get_theme_stylebox("hover")
+		if normal_style is StyleBoxTexture:
+			normal_style.texture = START_BUTTON
+			hover_style.texture = START_BUTTON
+			timer_button.position.y -= 5
+			animation_player.play("RESET")
 
 func _on_reset_button_pressed() -> void:
 	Reset_Timer()
