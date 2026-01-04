92e6a952c0ad7fc6720be8fcfaa1814d80450d52
Jonathan Vasquez <vasquez_jonathan@wheatoncollege.edu>
2026-01-04 14:33:52 -0500

    Default gif working and playing on startup

--- scripts/artwork.gd
+++ scripts/artwork.gd
@@ -1,10 +1,22 @@
 extends Node2D
+@onready var sprite: Sprite2D = $Sprite2D
+const POKEMON_WATERFALL_SPRITESHEET = preload("res://gifs/Pokemon Waterfall Spritesheet.png")
+const POKEMON_WATERFALL = preload("res://gifs/Pokemon Waterfall.png")
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	sprite.texture = POKEMON_WATERFALL_SPRITESHEET
+	sprite.hframes = 7
+	sprite.vframes = 6
+	sprite.frame = 0
+	
+	anim_sprite.offset = Vector2(0,0)
+	anim_sprite.centered = false
+	anim_sprite.scale = Vector2(0.32,0.32)
+	anim_sprite.play("default")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
